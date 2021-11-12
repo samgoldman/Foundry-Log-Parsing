3dd61e074c13d84b3d7aba7bc9e854a5d0807d76
Sam Goldman <sgoldman216@gmail.com>
2021-11-11 23:12:16 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,100 @@
+import sys
+from os import listdir
+from os.path import isfile, join
+from prettytable import PrettyTable
+
+from message import Message
+
+MESSAGE_DELIMITER = "---------------------------"
+
+
+def speaker_count(messages):
+    count_by_speaker = {}
+
+    for message in messages:
+        speaker = message.get_speaker()
+        if speaker not in count_by_speaker:
+            count_by_speaker[speaker] = 0
+        count_by_speaker[speaker] += 1
+    
+    return count_by_speaker
+
+
+def parse_files(filename):
+    message_lines = []
+    messages = []
+    with open(filename) as f:
+        for line in f:
+            line = line.strip()
+            if line == MESSAGE_DELIMITER:
+                messages.append(Message(message_lines))
+                message_lines.clear()
+            else:
+                message_lines.append(line)
+
+    # Last message
+    if len(message_lines) > 0:
+        messages.append(Message(message_lines))
+
+    return messages
+
+
+
+
+dirname = sys.argv[1]
+filenames = [join(dirname, f) for f in listdir(dirname) if isfile(join(dirname, f))]
+filenames = list(filter(lambda x: 'fvtt' in x, filenames))
+
+messages = []
+for filename in filenames:
+    messages += parse_files(filename)
+
+unique_messages = list(set(messages))
+
+speaker_assignments_filename = join(dirname, 'speaker_assignments.csv')
+rolls_by_player = {}
+speakers_to_player = {}
+with open(speaker_assignments_filename) as f:
+    for line in f:
+        split = line.strip().split(',')
+        player = split[0]
+        
+        rolls_by_player[player] = []
+        for s in split:
+            speakers_to_player[s] = player
+
+d20_rolls = list(filter(lambda m: m.is_d20_roll(), unique_messages))
+print(f"Total d20 Rolls: {len(d20_rolls)}")
+
+for roll in d20_rolls:
+    player = speakers_to_player[roll.get_speaker()]
+
+    rolls_by_player[player].append(roll)
+
+print()
+
+table = PrettyTable()
+
+table.field_names = ["Player", "Total Rolls", "Average Raw Roll", "Average Total Roll", "Natural 20s", "% Natural 20s", "Natural 1s", "% Natural 1s"]
+table.float_format["Average Raw Roll"] = "2.2"
+table.float_format["Average Total Roll"] = "2.2"
+
+for (player, rolls) in rolls_by_player.items():
+    num_rolls = len(rolls)
+    avg_raw_roll = sum(map(lambda r: r.get_d20_value(), rolls)) / num_rolls
+    avg_tot_roll = sum(map(lambda r: r.get_result(), rolls)) / num_rolls
+    nat_20_count = len(list(filter(lambda r: r.get_d20_value() == 20, rolls)))
+    nat_01_count = len(list(filter(lambda r: r.get_d20_value() ==  1, rolls)))
+    prcnt_nat_20 = nat_20_count / num_rolls
+    prcnt_nat_01 = nat_01_count / num_rolls
+
+    table.add_row([player, num_rolls, avg_raw_roll, avg_tot_roll, nat_20_count, f"{prcnt_nat_20*100:2.2}%", nat_01_count, f"{prcnt_nat_01*100:2.2}%"])
+
+table.reversesort = True
+print(table.get_string(sortby="Average Raw Roll"))
+
+
+
+
+
+
---
+++ message.py
@@ -0,0 +1,135 @@
+import re
+
+from time import strptime
+
+RE_TIMESTAMP_LINE = re.compile(r"\[(\d{1,2}/\d{1,2}/\d{4}, \d{1,2}:\d{2}:\d{2} [AP]M)\](.*)")
+
+def parse_timestamp(timestamp):
+    return strptime(timestamp, "%m/%d/%Y, %I:%M:%S %p")
+
+def parse_timestamp_line(line):
+    ''' Returns (timestamp, speaker)'''
+
+    match = RE_TIMESTAMP_LINE.match(line)
+    
+    if match is None:
+        return None
+
+    return (parse_timestamp(match.group(1)), match.group(2).strip())
+
+def parse_roll(lines):
+    if len(lines) < 2:
+        return None
+
+    # Expect first line to be a number
+    match = re.match(r"^\d+$", lines[0])
+
+    if match is None:
+        return None
+
+    possible_result = int(lines[0])
+
+    formula_match = None
+    for line in lines[1:]:
+        formula_match = re.match(r"(.+) = (.+) = (\d+)", line)
+        if not formula_match is None:
+            break
+
+    if formula_match is None:
+        return None
+
+    if int(formula_match.group(3)) != possible_result:
+        print(f"Huh, formula result doesn't match 2nd line result: ({formula_match.group(3)}, {possible_result})")
+        return None # Don't know what this is
+
+    part_1_split = formula_match.group(1).split(" ")
+    part_2_split = formula_match.group(2).split(" ")
+
+    assert(len(part_1_split) == len(part_2_split))
+
+    dice_rolled = []
+    dice_values = []
+
+    for (formula, result) in zip(part_1_split, part_2_split):
+        if 'd' in formula:
+            dice_rolled.append(formula)
+            dice_values.append(int(result))
+
+    return (possible_result, dice_rolled, dice_values)
+
+class Message:
+    def __init__(self, lines):
+        if len(lines) == 0:
+            raise Exception("No lines provided for message")
+
+        self.raw = " ".join(lines)
+
+        res = parse_timestamp_line(lines.pop(0))
+        if res is None:
+            raise Exception(f"First line of message does not match expected format: {self.raw}")
+        
+        (timestamp, speaker) = res
+        self.timestamp = timestamp
+        self.speaker = speaker
+
+        roll_parsed = parse_roll(lines)
+        if roll_parsed is None:
+            self.is_roll = False
+            self.roll_result = None
+            self.dice_rolled = None
+            self.dice_values = None
+        else:
+            self.is_roll = True
+            (self.roll_result, self.dice_rolled, self.dice_values) = roll_parsed
+
+    def is_d20_roll(self):
+        return self.is_roll \
+            and len(self.dice_rolled) == 1 \
+            and (self.dice_rolled[0] == '1d20' \
+                 or self.dice_rolled[0] == '2d20kl' \
+                 or self.dice_rolled[0] == '2d20kh')
+
+    def is_advantage(self):
+        return self.is_roll \
+            and len(self.dice_rolled) == 1 \
+            and self.dice_rolled[0] == '2d20kh'
+
+    def is_disadvantage(self):
+        return self.is_roll \
+            and len(self.dice_rolled) == 1 \
+            and self.dice_rolled[0] == '2d20kl'
+
+    def get_d20_value(self):
+        if self.is_d20_roll():
+            return self.dice_values[0]
+        else:
+            return 0
+
+    def get_message_text(self):
+        return self.raw
+
+    def get_speaker(self):
+        return self.speaker
+    
+    def get_dice_rolled(self):
+        return self.dice_rolled
+
+    def get_dice_results(self):
+        return self.dice_values
+
+    def get_result(self):
+        return self.roll_result
+
+    def get_timestamp(self):
+        return self.timestamp
+
+    def __key(self):
+        return self.raw
+
+    def __hash__(self):
+        return hash(self.__key())
+
+    def __eq__(self, other):
+        if isinstance(other, Message):
+            return self.__key() == other.__key()
+        return NotImplemented
